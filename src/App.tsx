bbcbb1404dc46ba7b27b0e3f482a9eb33f9846fc
King-navi <168604098+King-Navi@users.noreply.github.com>
2025-12-15 23:26:21 -0600

    Fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Home } from "./app/Home/pages/Home";
 import { SettingsPage } from "./app/Setting/pages/SettingsPage";
-import { RunModelPage } from "./app/RunModel/RunModelPage";
+import { RunModelPage } from "./app/RunModel/pages/RunModelPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
